c8deaa107c5b37f0924fc0ccd0f7d0c0fb091f31
shekhar <singhskr7@gmail.com>
2023-07-26 23:28:26 +0530

    change

---
+++ Project 2/src/component/navbar.jsx
@@ -0,0 +1,17 @@
+
+const Navbar = () => {
+    return (
+        <nav className="container ">
+            <div className="logo">
+                <img src="/image/logo.png" alt="logo" />
+            </div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
